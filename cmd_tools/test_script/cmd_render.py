6c7905c054921307a0c7c32588a02dee94399798
Spencer Au <spencer.au@amd.com>
2024-07-26 11:00:27 -0700

    works with 4.1 addon build

---
+++ cmd_tools/test_script/cmd_render.py
@@ -0,0 +1,98 @@
+import importlib
+# clear cache
+importlib.invalidate_caches()
+
+import os
+import subprocess
+import shutil
+import sys
+from dotenv import load_dotenv
+import zipfile
+
+
+def extract_addon_to_module(blender_path, blender_version):
+    target_dir = os.path.join(blender_path, blender_version, 'scripts', 'modules')
+    
+    if not os.path.exists(target_dir):
+        raise FileNotFoundError(f"Target directory {target_dir} does not exist.")
+    
+    # Extract the ZIP file to the target directory
+    with zipfile.ZipFile(addon, 'r') as zip_ref:
+        zip_ref.extractall(target_dir)
+
+    print(f"Addon extracted to {target_dir}")
+
+
+def remove_rprblender(blender_path, blender_version):
+    target_dir = os.path.join(blender_path, blender_version, 'scripts', 'modules', 'rprblender')
+
+    try:
+        shutil.rmtree(target_dir, ignore_errors=True)
+        print(f"rprblender removed successfully from {target_dir}.")
+    except Exception as e:
+        print(f"Error removing rprblender: {e}")
+
+
+def print_sys_path():
+    print("SYS.PATH FOR cmd_render.py")
+    for i, path in enumerate(sys.path):
+        print(f"{i}: {path}")
+
+
+if __name__ == "__main__":
+
+    load_dotenv()
+
+    sys.path.append("./rprblender")
+    script = sys.argv[1]
+    blender_path = os.getenv('BLENDER_PATH')
+    blender_version = os.path.basename(blender_path).split()[-1]
+    blender_exe = os.path.join(blender_path, "blender.exe")
+    addon = os.getenv('ADDON_ZIP')
+    blender_files = os.getenv('SCENE_PATH')
+    scene = os.getenv('SCENE_NAME')
+    ground_truth = os.getenv('GROUND_TRUTH')
+    viewport_flag = os.getenv('VIEWPORT_FLAG')
+
+    print(f"Blender Version: {blender_version}")
+    print(f"Scene name: {scene}")
+
+    # extract zip file to blender's modules subdir in scripts
+    extract_addon_to_module(blender_path, blender_version)
+
+    # Always run final_render.py
+    final_render_command = [
+        blender_exe,
+        '--background',
+        '--python', script,
+        blender_files,
+        scene,
+        addon,
+        blender_path,
+        blender_version
+
+    ]
+    subprocess.run(final_render_command)
+    
+    # Always run compare_render.py after final_render.py
+    compare_render_command = [
+        'python', 'compare_render.py',
+        '--ground-truth-dir', ground_truth,
+        '--output-dir', scene,
+        '--scene-name', scene
+    ]
+    subprocess.run(compare_render_command)
+
+    # Conditionally run viewport render
+    viewport_render_command = [
+        blender_path,
+        '--python', 'viewport_render.py',
+        '--',
+        '--scene-path', blender_files,
+        '--scene-name', scene,
+        '--addon-zip', addon
+    ]
+    if viewport_flag == 1:
+        subprocess.check_call(viewport_render_command)
+
+    remove_rprblender(blender_path, blender_version)
---
+++ cmd_tools/test_script/final_render.py
@@ -0,0 +1,68 @@
+import os
+import shutil
+import zipfile
+import importlib
+from pathlib import Path
+import sys
+
+
+def create_output_dir(scene_name):
+    output_dir = os.path.abspath(scene_name)
+
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+
+    os.makedirs(output_dir)
+
+    return output_dir
+
+
+def print_sys_path():
+    print("SYS.PATH FOR final_render.py")
+    for i, path in enumerate(sys.path):
+        print(f"{i}: {path}")
+
+
+def render_final_image(blender_files, scene):
+
+    import bpy
+    output_dir = create_output_dir(scene_name=scene)
+
+    bpy.context.scene.render.engine = 'RPR'
+    temp_path = os.path.join(blender_files, scene + ".blend")
+    scene_path = os.path.abspath(temp_path)
+
+    bpy.ops.wm.open_mainfile(filepath=scene_path)
+    bpy.context.scene.rpr.final_render_mode = 'FULL2'  # Set Render Mode to Final
+    bpy.context.scene.render.filepath = os.path.join(output_dir, scene + "_final.png")
+    bpy.ops.render.render(write_still=True)
+    print(f"{scene} rendered successfully at {output_dir}")
+
+
+def install_and_enable_addon():
+    # Set up the addon paths
+    src_path = str((Path(__file__).parent.parent.parent/'src').resolve())
+
+    #import sys
+    if src_path not in sys.path:
+        sys.path.append(src_path)
+
+    import rprblender
+    rprblender.register()
+
+
+def main():
+
+    blender_files = sys.argv[4]
+    scene = sys.argv[5]
+
+    #install_and_enable_addon()
+    import rprblender
+    rprblender.register()
+
+    render_final_image(blender_files, scene)
+
+
+if __name__ == "__main__":
+  
+    main()
